69127e75dda5257072dc186b9a0bc2cd03b05381
Prathibha Ratnayake <prsthibha@gmail.com>
2023-09-18 10:27:13 +0530

    Add auth setup

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,7 +1,9 @@
-import Image from 'next/image'
+import { UserButton } from '@clerk/nextjs';
 
 export default function Home() {
   return (
-    <p className='text-2xl text-indigo-500'>Hello world</p>
-  )
+    <div>
+      <UserButton afterSignOutUrl='/' />
+    </div>
+  );
 }
